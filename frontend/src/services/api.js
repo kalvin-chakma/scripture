4faf6d75cd0d1f9a0f3731c288b0af76617e00c2
Kalvin Chakma <123386698+kalvin-chakma@users.noreply.github.com>
2025-06-07 05:59:26 +0600

    update backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+//const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API = "https://scripture-xi.vercel.app/";
 
 const apiClient = axios.create({
   baseURL: API,
