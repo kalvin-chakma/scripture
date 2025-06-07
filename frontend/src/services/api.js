7d0a8c114be4e42ba186f8113ccb7bd97b61db5e
Kalvin Chakma <123386698+kalvin-chakma@users.noreply.github.com>
2025-06-07 07:21:02 +0600

    fixed cors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
-//const API = "https://scripture-xi.vercel.app/";
+//const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API = "https://scripture-xi.vercel.app/";
 
 const apiClient = axios.create({
   baseURL: API,
