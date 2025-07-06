cfda94f4e5011529f970bd2bcd531aa4e970f0b6
Kalvin Chakma <123386698+kalvin-chakma@users.noreply.github.com>
2025-07-06 13:50:49 +0600

    Added dark theme styles for NoteForm and NoteModel

--- frontend/src/components/ui/NoteForm.jsx
+++ frontend/src/components/ui/NoteForm.jsx
@@ -24,12 +24,17 @@ export default function NoteForm({ onClose }) {
     <form onSubmit={handleSubmit}>
       {/* Note Type */}
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Note Type</label>
+        <label className="block text-sm font-medium mb-1 dark:text-white ">
+          Note Type
+        </label>
         <select
-          className="w-full border border-gray-300 rounded px-3 py-2"
+          className="w-full border text-sm border-gray-300 dark:border-none rounded px-3 py-2 dark:bg-neutral-900 dark:text-gray-200 dark:border-gray-600"
           value={noteType}
           onChange={(e) => setNoteType(e.target.value)}
         >
+          <option value="markdown">Markdown</option>
+          <option value="general">General</option>
+          <option value="todo">To-do</option>
           <option value="markdown">Markdown</option>
           <option value="general">General</option>
           <option value="todo">To-do</option>
@@ -41,7 +46,7 @@ export default function NoteForm({ onClose }) {
         <label className="block text-sm font-medium mb-1">Title</label>
         <input
           type="text"
-          className="w-full border border-gray-300 rounded px-3 py-2"
+          className="w-full border text-sm border-gray-300 rounded px-3 py-2 dark:bg-neutral-900 dark:text-gray-400 dark:border-gray-600"
           placeholder="Enter note title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
@@ -49,17 +54,16 @@ export default function NoteForm({ onClose }) {
       </div>
 
       {/* Actions */}
-      <div className="flex justify-end">
+      <div className="flex flex-col sm:flex-row justify-end sm:space-x-2 space-y-2 sm:space-y-0">
         <button
-          type="button"
           onClick={onClose}
-          className="bg-gray-200 text-gray-700 px-4 py-2 rounded hover:bg-gray-300"
+          className="flex items-center justify-center bg-gray-500 text-sm font-semibold text-gray-100 dark:text-gray-300 px-4 py-2 rounded hover:bg-gray-600 cursor-pointer h-[4vh] w-full sm:w-24"
         >
           Cancel
         </button>
         <button
           type="submit"
-          className="ml-2 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+          className="flex items-center justify-center bg-green-700 text-sm font-semibold text-gray-100 dark:text-gray-300 px-4 py-2 rounded hover:bg-green-800 cursor-pointer h-[4vh] w-full sm:w-24"
         >
           Save
         </button>
--- frontend/src/components/ui/NoteModal.jsx
+++ frontend/src/components/ui/NoteModal.jsx
@@ -3,7 +3,7 @@ import NoteForm from "./NoteForm";
 export default function NoteModal({ onClose }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50 p-4">
-      <div className="bg-white p-6 rounded shadow-lg w-full max-w-xs lg:max-w-md">
+      <div className="bg-white dark:bg-black dark:text-white p-6 rounded shadow-lg w-full max-w-xs lg:max-w-md">
         <h2 className="text-lg font-semibold mb-4 text-center">
           Create a Note
         </h2>
